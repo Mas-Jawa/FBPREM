62880832b3935edac6df6aa4dc2dcc9ae273d61b
EvilRat <fendipendol124@gmail.com>
2025-02-22 17:43:23 +0700

    Add files via upload

--- Fen-FBV1.py
+++ Fen-FBV1.py
@@ -101,6 +101,18 @@ from rich.markdown import Markdown as mark
 from concurrent.futures import ThreadPoolExecutor as tred
 from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
 
+#------[DATA SOUND/MODULE]------#
+os.system("pkg install espeak")
+os.system('clear')
+time.sleep(2)
+os.system('espeak -a 300 " CHECK!!"')
+print('\033[97;1m[\x1b[38;5;50m+\033[97;1m] \x1b[38;5;50mCHECK!!')
+time.sleep(4)
+os.system('espeak -a 300 " WELCOME"')
+print('\033[97;1m[\x1b[38;5;50m+\033[97;1m] \x1b[38;5;50mWELCOME')
+time.sleep(2)
+os.system('clear')
+
 # GLOBAL NAME
 sekarang = calendar.timegm(time.gmtime(time.time()))
 pretty.install()
@@ -117,31 +129,35 @@ id, id2, loop, ok, cp, akun, tokenku, uid, method, pwpluss, pwnya, tokenmu = [],
 dia, ualu, ualuh = [], [], []
 sys.stdout.write("\x1b]2; fendi ganteng\x07")
 
-#------[PEMBUKAAAN]------#
-def banner():
-    os.system('clear')
-    cetak(panel(f'''[blod yellow])
-[bold yellow] ____  _   _ ____  _     _____ _____ ____  _   _ 
-[blod yellow]|  _ \| \ | |  _ \| |   | ____| ____|  _ \| \ | |
-[blod yellow]| | | |  \| | | | | |   |  _| |  _| | |_) |  \| |
-[blod yellow]| |_| | |\  | |_| | |___| |___| |___|  _ <| |\  |
-[bold yellow]|____/|_| \_|____/|_____|_____|_____|_| \_\_| \_|
-[blue]AUTOR : FENDI 
-[blue]VERSION : 1.0
-[blue]STATUS : PREMIUM
-[blue]INSTAGRAM : @fendipendol65                ''',width=68,title=f"[[green] FENDI GANTENG [/]]",padding=(0,2),style=f"bold green"))
-
-#------[DATA SOUND/MODULE]------#
-os.system("pkg install espeak")
-os.system('clear')
-time.sleep(2)
-os.system('espeak -a 300 " CHECK!!"')
-print('\033[97;1m[\x1b[38;5;50m+\033[97;1m] \x1b[38;5;50mCHECK!!')
-time.sleep(4)
-os.system('espeak -a 300 " WELCOME"')
-print('\033[97;1m[\x1b[38;5;50m+\033[97;1m] \x1b[38;5;50mWELCOME')
-time.sleep(2)
-os.system('clear')
+#<----------[ WARNA ]---------->#
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+mahal = random.choice([P,M,K,H,B,U])
+#<----------[ BULAN ]---------->#
+rb = {'1':'JANUARI','2':'FEBRUARI','3':'MARET','4':'APRIL','5':'MEI','6':'JUNI','7':'JULI','8':'AGUSTUS','9':'SEPTEMBER','10':'OKTOBER','11':'NOVEMBER','12':'DESEMBER'}
+tg = datetime.datetime.now().day
+bl = rb[(str(datetime.datetime.now().month))]
+th = datetime.datetime.now().year
+oke = 'FENN-LIVE-'+str(tg)+'-'+str(bl)+'-'+str(th)+'.txt'
+cpe = 'FENN-CHECK-'+str(tg)+'-'+str(bl)+'-'+str(th)+'.txt'
+#<----------[ UCAPAN MANIS ]---------->#
+hour = datetime.datetime.now().hour
+if 19 <= hour < 4:
+  sall2 = f"SELAMAT MALAM"
+elif 4 <= hour < 12:
+  sall2 = f"SELAMAT PAGI"
+elif 12 <= hour < 15:
+  sall2 = "SELAMAT SIANG"
+elif 15 <= hour < 18:
+  sall2 = f"SELAMAT SORE"
+elif 18 <= hour < 19:
+  sall2 = f"SELAMAT MALAM"
+else:
+  sall2 = f"SELAMAT MALAM"  
 
 #------[PRXY]------#
 try:
@@ -155,3 +171,26 @@ except Exception as e:
     )
     exit()
 prox = open(".prox.txt", "r").read().splitlines()
+
+#<----------[ ANIMASI ]---------->#
+def fenn(berjalan):
+        for gasle in berjalan + "\n":sys.stdout.write(gasle);sys.stdout.flush();time.sleep(0.05)
+def fenn(berjalan):
+        for gasle in berjalan + "\n":sys.stdout.write(gasle);sys.stdout.flush();time.sleep(0.01)
+def clear():
+	os.system('clear')
+def back():
+	menu(id)
+#------[PEMBUKAAAN]------#
+def banner():
+    os.system('clear')
+    cetak(panel(f'''[blod yellow])
+[bold yellow] ____  _   _ ____  _     _____ _____ ____  _   _ 
+[blod yellow]|  _ \| \ | |  _ \| |   | ____| ____|  _ \| \ | |
+[blod yellow]| | | |  \| | | | | |   |  _| |  _| | |_) |  \| |
+[blod yellow]| |_| | |\  | |_| | |___| |___| |___|  _ <| |\  |
+[bold yellow]|____/|_| \_|____/|_____|_____|_____|_| \_\_| \_|
+[blue]AUTOR : FENDI 
+[blue]VERSION : 1.0
+[blue]STATUS : PREMIUM
+[blue]INSTAGRAM : @fendipendol65                ''',width=68,title=f"[[green] FENDI GANTENG [/]]",padding=(0,2),style=f"bold green"))
